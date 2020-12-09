30117d3907c6e97c471c98fa4032950a513128cc
IsagulovUrmat <falone641234@gmail.com>
2020-12-09 21:24:18 +0600

    main_code_of_task

--- TASKS.py
+++ TASKS.py
@@ -12,18 +12,34 @@
 #     else:
 #         print('IGNORE HIM!')
 
-n = int(input())
-h = int(input())
-i = 0
-y = 0
-while i < n:
-    rost = int(input())
-    if rost > h:
-        y = y + 2
-    else:
-        y = y + 1
-    i = i + 1
-print(y)
+#https://codeforces.com/problemset/problem/677/A
+                #Решение 1
+# n = int(input())
+# h = int(input())
+# i = 0
+# y = 0
+# while i < n:
+#     rost = int(input())
+#     if rost > h:
+#         y = y + 2
+#     else:
+#         y = y + 1
+#     i = i + 1
+# print(y)
+                    #РЕШЕНИЕ 2.
+# string = input().split()
+# print(string)
+# n = int(string[0])
+# h = int(string[1])
+# list1 = list(map(int, input().split()))
+# lat = 0
+# for student in list1:
+#     if student > n:
+#         lat += 2
+#     else:
+#         lat += 1
+# print(lat)
+
 
 
 
