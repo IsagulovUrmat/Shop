052972cbd5dc5516d94008b2c28125e0c751ee04
IsagulovUrmat <falone641234@gmail.com>
2020-12-09 21:01:17 +0600

    main_code_of_task

--- TASKS.py
+++ TASKS.py
@@ -1,12 +1,31 @@
-name = 'Aleksei'
+#если кол-во не похожих элементов четно, выввести ЧАТ ВИЗ ХЕ.
 
-if len(name) < 100:
-    list2 = []
-    for i in name:
-        if i not in list2:
-            list2.append(i)
+# name = 'Aleksei'
+#
+# if len(name) < 100:
+#     list2 = []
+#     for i in name:
+#         if i not in list2:
+#             list2.append(i)
+#     if len(list2) % 2 == 0:
+#         print('CHAT WITH HER!')
+#     else:
+#         print('IGNORE HIM!')
 
-    if len(list2) % 2 == 0:
-        print('CHAT WITH HER!')
+n = int(input())
+h = int(input())
+i = 0
+y = 0
+while i < n:
+    rost = int(input())
+    if rost > h:
+        y = y + 2
     else:
-        print('IGNORE HIM!')
+        y = y + 1
+    i = i + 1
+print(y)
+
+
+
+
+
