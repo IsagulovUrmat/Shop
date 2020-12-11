b4228be914b753769cdc232d5edee2d7beade19d
IsagulovUrmat <falone641234@gmail.com>
2020-12-11 21:05:37 +0600

    main_code_of_task

--- TASKS.py
+++ TASKS.py
@@ -40,6 +40,63 @@
 #         lat += 1
 # print(lat)
 
+#ZADACHA: https://codeforces.com/problemset/problem/581/A
+
+#REWENIE1
+
+# a, b = input().split()
+# a = int(a)
+# b = int(b)
+
+# print(min(a,b), (max(a,b) - min(a,b)) // 2)
+
+#REWENIE2
+
+# socks = list(map(int,input().split()))
+# a = socks[0]
+# b = socks[1]
+# count_fash = 0
+# count_notfash = 0
+# if a > b:
+#     a = a - b
+#     if a > 1:
+#         count_notfash = a // 2
+#     print(b, count_notfash)
+# elif a < b:
+#     b = b - a
+#     if b > 1:
+#         count_notfash = b // 2
+#         print(a, count_notfash)
+#     elif b == 1:
+#         b = a
+#         print(b, count_notfash)
+# elif a == b:
+#     a = a + b
+#     a = a//2
+#     print(a, count_notfash)
+
+#ZADACHA: https://codeforces.com/problemset/problem/381/A
+
+n = int(input())
+list1 = list(map(int,input().split()))
+ser = 0
+dim = 0
+i = 0
+while i < n:
+    scales = max(list1[0],list1[-1])
+    if i % 2 == 0:
+        ser += scales
+    else:
+        dim += scales
+    i += 1
+    list1.remove(scales)
+print(ser,dim)
+
+
+
+
+
+
 
 
 
